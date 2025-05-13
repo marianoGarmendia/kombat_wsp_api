baa3532c124e30c27fe2f183b4155445007de232
Mariano Garmendia <marianogarmendia77@gmail,com>
2025-05-13 13:17:33 -0300

    Prompt para hot sale

--- prompt_original.ts
+++ prompt_original.ts
@@ -1,94 +1,4 @@
-import { TavilySearchResults ,TavilySearchAPIRetrieverFields} from "@langchain/community/tools/tavily_search";
-import {
-  AIMessage,
-  SystemMessage
-} from "@langchain/core/messages";
-import { tool } from "@langchain/core/tools";
-import { Annotation, MemorySaver, MessagesAnnotation, StateGraph } from "@langchain/langgraph";
-import { ToolNode } from "@langchain/langgraph/prebuilt";
-import { ChatOpenAI } from "@langchain/openai";
-import { z } from "zod";
-
-// process.env.LANGCHAIN_CALLBACKS_BACKGROUND = "true";
-import * as dotenv from "dotenv";
-import { TavilySearchAPIRetriever } from "@langchain/community/retrievers/tavily_search_api";
-dotenv.config();
-
-// const tavilySearch = new TavilySearchAPIRetriever({
-//   apiKey: process.env.TAVILY_API_KEY,
-//   includeDomains:["https://kombatpadel.com.ar/es/palas-padel","https://kombatpadel.com.ar/es/palas-formato-diamante","https://kombatpadel.com.ar/es/palas-formato-redondo","https://kombatpadel.com.ar/es/palas-formato-lagrima","https://kombatpadel.com.ar/es/nueva-coleccion-vulcano","https://kombatpadel.com.ar/es/kombat/test-point-kombat"],
-//   searchDepth: "advanced",
-// });
-
-const tavilySearch = new TavilySearchAPIRetriever({
-  apiKey: process.env.TAVILY_API_KEY,
-  includeDomains:["https://kombatpadel.com.ar/es/kombat/hotsale"],
-  searchDepth: "advanced",
-});
-
-const tavily = tavilySearch.asTool({
-  name: "tavilySearch",
-  description: "Utiliza esta herramienta cuando consulten por precios o promociones, esta semana es HOT SALE asi que debes redirigir a la pagina de promociones",
- 
- 
-  schema: z.string()
-
-})
-
-// const ejecutar_widget_api = tool(
-//   async ({ wa_id, cel_number }) => {
-//     await fetch("https://urbanevents.techbank.ai:4001/whatsapp/ejecutar_widget", {})
-//   }
-//   , {
-//     name: "ejecutar_widget_api",
-//     description: "Utiliza esta herramienta cuando... ",
-//     schema: z.object({
-//       wa_id: z.string(),
-//       cel_number: z.string(),
-//     }),
-//   })
-
-
-// const tools = [];
-// for (const t of tools) {
-//   if (t === 'tavilySearch') tools.push(tavilySearch);
-//   if (t === 'ejecutar_widget_api') tools.push(ejecutar_widget_api);
-// }
-const tools = [tavily];
-
-const stateAnnotation = MessagesAnnotation;
-
-const contextAnnotation = Annotation.Root({
-  context: Annotation<string>,
-  cel_number: Annotation<string>,
-  authentication: Annotation<boolean>,
-  ...stateAnnotation.spec
-});
-
-export const model = new ChatOpenAI({
-  model: "gpt-4o",
-  apiKey: process.env.OPENAI_API_KEY_WIN_2_WIN,
-  temperature: 0,
-}).bindTools(tools);
-
-const toolNode = new ToolNode(tools);
-
-async function callModel(state: typeof contextAnnotation.State) {
-  const { messages, context } = state;
-
-
-// WIN 2 WIN
-  // const systemsMessage = new SystemMessage(
-  //   `
-  //       ${context}
-    
-  //     ### Información de horario
-  //     El dia de hoy es ${new Date().toLocaleDateString()} y la hora actual es ${new Date().toLocaleTimeString()}.
-  //     `
-  // );
-
-  const systemsMessage = new SystemMessage(`
-    Eres un agente de inteligencia artificial diseñado para actuar como representante de Kombat, una marca líder en el mercado de artículos de pádel. Tu objetivo es proporcionar información precisa y actualizada sobre los productos y servicios de Kombat, asistiendo a los clientes de manera profesional y amistosa, como lo haría un asesor de ventas de la marca.
+const systemprompt_original = `  Eres un agente de inteligencia artificial diseñado para actuar como representante de Kombat, una marca líder en el mercado de artículos de pádel. Tu objetivo es proporcionar información precisa y actualizada sobre los productos y servicios de Kombat, asistiendo a los clientes de manera profesional y amistosa, como lo haría un asesor de ventas de la marca.
 
   La siguiente información es oficial y debe ser priorizada por sobre cualquier otra que pueda obtenerse desde herramientas o internet. No debe ser contradicha ni reemplazada.
 
@@ -104,7 +14,11 @@ respuesta: El tiempo de entrega de los productos es de 2 a 7 días hábiles. Una
 
 cliente :Qué descuentos o promos hay?
 Respuesta: 
-Ésta semana hay HOT SALE, te invito a visitar la página ""https://kombatpadel.com.ar/es/kombat/hotsale" donde vas a encontrar ofertas solo por tiempo limitado.
+-Actualmente, tenemos las siguientes promociones:
+- Hasta 12 cuotas sin interés con tarjetas de crédito bancarizadas.
+- 20% de descuento en pagos con transferencia o efectivo.
+- $110000 de descuento al comprar un bolso y una pala juntos.
+Además, te adelanto que en mayo tendremos un Hot Sale. Si necesitas más detalles sobre alguna de estas promociones, házmelo saber. (NO ADELANTAR PRÓXIMAS PROMOS)
 
 cliente: ¿Cómo hago un pedido?
  Respuesta: Para realizar un pedido con Kombat, puedes seguir estos pasos generales:
@@ -116,7 +30,8 @@ cliente: ¿Cómo hago un pedido?
 Si necesitas más detalles o asistencia personalizada, puedes comunicarte con el servicio de atención al cliente de Kombat.
 
 cliente:  ¿Cuál es el precio del bolso Aqua?
-respuesta: Para precios y promociones visita "https://kombatpadel.com.ar/es/kombat/hotsale" ya que es semana de descuentos asi podes aprovechar el tuyo.
+respuesta: El bolso Aqua vale 159999 con transferencia, sino también lo podés comprar en hasta 12 cuotas sin interés de 16667. Te dejo el link para que lo veas con sus detalles:
+https://kombatpadel.com.ar/es/inicio/bolso-paletero-aqua-x-manu-martin
 
 cliente:  ¿Qué pala me recomendás? Soy jugador de drive, estratega, armo el juego.
 respuesta: Para un jugador de drive que se enfoca en la estrategia y en armar el juego, te recomendaría una pala que ofrezca un buen equilibrio entre control y potencia. Las palas con forma de lágrima o redonda suelen ser ideales para este estilo de juego, ya que proporcionan un excelente control y precisión.
@@ -242,58 +157,4 @@ Instagram: @kombatpadelargentina​
 - Tu objetivo es dar información y cerrar una venta o al menos llevar al cliente a elegir un producto de kombat de manera persuasiva y amigable.
 
   INFORMACIÓN CONTEXTUAL de dia y hora:
-  Hoy es ${new Date()} y la hora es ${new Date().toLocaleTimeString()} 
-  -
-  
-    `)
-
-// @ts-expect-error
-  const response = await model.invoke([systemsMessage, ...messages]);
-
-  // We return a list, because this will get added to the existing list
-  return { messages: [response] };
-}
-
-function checkToolCall(state: typeof contextAnnotation.State) {
-  const { messages } = state;
-  console.log("Ckeck tool call");
-
-  console.log(messages);
-
-  const lastMessage = messages[messages.length - 1] as AIMessage;
-
-  // If the LLM makes a tool call, then we route to the "tools" node
-  if (lastMessage?.tool_calls?.length) {
-    return "tools";
-  } else {
-    return "__end__";
-  }
-
-  // Otherwise, we stop (reply to the user)
-}
-const graph = new StateGraph(contextAnnotation);
-
-
-graph
-  .addNode("agent", callModel)
-  .addNode("tools", toolNode)
-  .addEdge("__start__", "agent")
-  .addConditionalEdges("agent", checkToolCall, ["tools", "__end__"])
-  .addEdge("tools", "agent");
-
-const checkpointer = new MemorySaver();
-
-// Implementacion agente interfazp personalizada
-export const workflow = graph.compile({ checkpointer });
-
-// Implementacion langgraph studio sin checkpointer
-// export const workflow = graph.compile();
-
-
-
-// const response = await workflow.invoke(
-//   { messages: "Hola, quiero saber que paso en bahia blanca en estos dias por la inundacion" },
-//   { configurable: { thread_id: "124" } }
-// );
-
-// console.log(response);
+  Hoy es ${new Date()} y la hora es ${new Date().toLocaleTimeString()} `
